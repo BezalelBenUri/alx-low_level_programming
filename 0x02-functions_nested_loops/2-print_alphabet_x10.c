7a01710fe656e28d37a930eb0c00b65089bd4080
PhenomenalAI <diamondinyang@gmail.com>
2023-02-21 07:50:36 -0800

    alphabt 2x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,19 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
- *
- * Return: void.
- */
+* print_alphabet_x10 - Prints the alphabet 10 times.
+*
+* Return: Always 0
+*/
 void print_alphabet_x10(void)
 {
-	int i = 0;
-	while (i < 10)
+	int i, a;
+
+		for (i = 0; i <= 9; i++)
 	{
-		print_alphabet();
-		i++;
+		for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+		_putchar('\n');
 	}
 }
