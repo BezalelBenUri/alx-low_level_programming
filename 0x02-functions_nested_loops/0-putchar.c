ddd3cf6e663159852c5e189872e3dfec106d607a
PhenomenalAI <diamondinyang@gmail.com>
2023-02-21 06:18:11 -0800

    adding header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
-* main - Print the word "_putchar"
-*
-* Return: Always 0 (Success)
-*/
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int i;
-	char c[] = "_putchar";
-
-	for (i = 0; i < 9; i++)
-		{
-		_putchar(c[i]);
-		}
-		_putchar('\n');
+	_putchar('_');
+    	_putchar('p');
+    	_putchar('u');
+    	_putchar('t');
+    	_putchar('c');
+    	_putchar('h');
+    	_putchar('a');
+    	_putchar('r');
+    	_putchar('\n');
+	
 	return (0);
 }
