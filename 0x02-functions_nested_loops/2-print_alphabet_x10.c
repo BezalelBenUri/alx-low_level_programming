41f4c0503b0c1f75c2e036fff14bf8310777cb3e
PhenomenalAI <diamondinyang@gmail.com>
2023-02-21 07:17:04 -0800

    alphabet 2x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
+ *
+ * Return: void.
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		_putchar();
+		i++;
+	}
+}
