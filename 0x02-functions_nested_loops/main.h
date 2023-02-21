ed491c26d8ac704b17658b06e50c3580c18c9b60
PhenomenalAI <diamondinyang@gmail.com>
2023-02-21 11:25:56 -0800

    adds two integers and returns the result

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,5 +11,12 @@ int _abs(int);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int)
+int add(int a, int b);
+void pr_with_fmt(int charcode, int fmt);
+void print_to_98(int n);
+void pr_3_digit(int charcode, int fmt);
+void print_times_table(int n);
+void print_spaces(int n);
+void print_zero(void);
+
 #endif
