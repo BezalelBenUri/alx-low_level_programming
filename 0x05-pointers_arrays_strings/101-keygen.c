faeb9c8dd84302f9476e594bcdab2a277045dee1
PhenomenalAI <diamondinyang@gmail.com>
2023-02-27 11:08:35 -0800

    keygen.  program that generates random valid passwords for the program 101-crackme

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+    char password[64];
+    int i, sum, diff;
+
+    srand(time(NULL));
+
+    // Generate 63 printable ASCII characters
+    for (i = 0, sum = 0; i < 63; i++) {
+        password[i] = rand() % 94 + 32; // printable ASCII range is 32 to 126
+        sum += password[i];
+    }
+
+    // Adjust the sum to be equal to 2772
+    diff = sum - 2772;
+    password[62] -= diff;
+
+    printf("%s", password);
+
+    return 0;
+}
