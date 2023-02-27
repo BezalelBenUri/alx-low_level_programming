c9da4b96a653042c6077a24049e0f2ee2212bb40
PhenomenalAI <diamondinyang@gmail.com>
2023-02-27 08:52:37 -0800

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,32 +7,32 @@
  */
 void rev_string(char *s)
 {
-  int length = 0, c;
-  char *begin, *end, temp;
-
-  /* Find the length of the string */
-  while (s[length] != '\0')
-    length++;
-
-  /* Set the begin and end pointers to the start
+	int length = 0, c;
+	char *begin, *end, temp;
+	
+	/* Find the length of the string */
+	while (s[length] != '\0')
+		length++;
+	
+	/* Set the begin and end pointers to the start
    * and end of the string */
-  begin = s;
-  end = s;
-
-  for (c = 0; c < length - 1; c++)
-    end++;
-
-  /* Swap the characters of the string */
-  for (c = 0; c < length / 2; c++)
-  {
-    temp = *end;
-    *end = *begin;
-    *begin = temp;
-
-    /* Update pointers to point to the next
+	begin = s;
+	end = s;
+	for (c = 0; c < length - 1; c++)
+		end++;
+	
+	/* Swap the characters of the string */
+	for (c = 0; c < length / 2; c++)
+	{
+		temp = *end;
+		*end = *begin;
+		*begin = temp;
+		
+		
+	/* Update pointers to point to the next
      * character in the string */
-    begin++;
-    end--;
+		begin++;
+		end--;
   }
 }
 
