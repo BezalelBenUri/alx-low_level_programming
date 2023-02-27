c9eece39f25d7d932eced8ebaabdc53e456cc1b2
PhenomenalAI <diamondinyang@gmail.com>
2023-02-27 11:23:07 -0800

    keygen.  program that generates random valid passwords for the program 101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,49 +4,16 @@
 
 int main(void)
 {
-    char password[16];
-    int i;
+    char password[16]; /* 15 characters plus null terminator */
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;':\",./<>?"; /* list of possible characters */
 
-    srand(time(NULL));
+    srand(time(NULL)); /* seed random number generator with current time */
 
-    /* Generate 15 random characters */
-    for (i = 0; i < 15; i++) {
-        int r = rand() % 62;
-        if (r < 26) {
-            /* Uppercase letter */
-            password[i] = 'A' + r;
-        } else if (r < 52) {
-            /* Lowercase letter */
-            password[i] = 'a' + (r - 26);
-        } else {
-            /* Digit */
-            password[i] = '0' + (r - 52);
-        }
+    for (int i = 0; i < 15; i++) {
+        password[i] = charset[rand() % (sizeof(charset) - 1)]; /* pick a random character from charset and add it to password */
     }
+    password[15] = '\0'; /* add null terminator to end of password */
 
-    /* Add null terminator */
-    password[15] = '\0';
-
-    /* Make sure the password contains at least one uppercase letter,*/
-    /* one lowercase letter, and one digi*/t
-    int has_uppercase = 0;
-    int has_lowercase = 0;
-    int has_digit = 0;
-    for (i = 0; i < 15; i++) {
-        if (password[i] >= 'A' && password[i] <= 'Z') {
-            has_uppercase = 1;
-        } else if (password[i] >= 'a' && password[i] <= 'z') {
-            has_lowercase = 1;
-        } else if (password[i] >= '0' && password[i] <= '9') {
-            has_digit = 1;
-        }
-    }
-    if (!has_uppercase || !has_lowercase || !has_digit) {
-        /* Regenerate password if it doesn't meet the requirements */
-        return main();
-    }
-
-    /* Print the password */
     printf("%s", password);
 
     return 0;
