923d9308900df01b4e9c400d9bef398292492b40
PhenomenalAI <diamondinyang@gmail.com>
2023-03-02 00:49:47 -0800

    cleansing

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,30 +1,30 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
- * @dest: destination string
- * @src: source string
+ * _strcat - Concatenates two strings.
+ * @dest: Pointer to destination string.
+ * @src: Pointer to source string.
  *
- * Return: pointer to resulting string
+ * Return: Pointer to resulting string.
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
-	
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	
-	dest[i] = '\0';
-	
-	return (dest);
+        int i = 0, j = 0;
+
+        while (dest[i] != '\0')
+        {
+                i++;
+        }
+
+        while (src[j] != '\0')
+        {
+                dest[i] = src[j];
+                i++;
+                j++;
+        }
+
+        dest[i] = '\0';
+
+        return (dest);
 }
+
