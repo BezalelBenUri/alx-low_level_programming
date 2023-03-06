41e6915921839479af8df8f247b1ae474d469c35
PhenomenalAI <diamondinyang@gmail.com>
2023-03-06 10:42:07 -0800

    function that locates a substring.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,12 +8,12 @@
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-int _strncmp(char *s1, char *s2, unsigned int b)
+int _putchar(char c);
 
 #endif /* MAIN_H */
