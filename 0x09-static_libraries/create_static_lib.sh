a6924fc3dc90480fc988df18c4c55093b08d8b8c
PhenomenalAI <diamondinyang@gmail.com>
2023-03-10 07:42:09 -0800

    script  that creates a static library called liball.a from all the .c files that are in the current directory

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compile all .c files into object files
-gcc -Wall -Werror -Wextra -pedantic -c *.c
+gcc -c *.c
 
 # Create the static library from the object files
 ar rc liball.a *.o
