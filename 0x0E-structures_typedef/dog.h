685c8ab2fed5c84b9695d06f0ca6d426388ee592
PhenomenalAI <diamondinyang@gmail.com>
2023-03-20 08:28:02 -0700

    new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct dog - Define a new struct dog with name, age, owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of dog
+ **/
+struct dog
+{
+	char *name;
+	char age;
+	char * owner;
+};
