8ee79542133d6f499aca8cf6bb0d491aadad2f19
PhenomenalAI <diamondinyang@gmail.com>
2023-04-03 12:49:59 -0700

    function that frees a listint_t list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -30,5 +30,7 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+void free_list3(listint_addr *head);
+listint_addr *add_nodeaddr2(listint_addr **head, const listint_t *addr);
 
 #endif
