09da75aaba4726a598258aa12bc895c7a4ebec84
PhenomenalAI <diamondinyang@gmail.com>
2023-04-07 05:38:00 -0700

    function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,33 +2,28 @@
 #include <stddef.h>
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
+ * binary_to_uint - Converts a binary number to an unsigned int
+ * @b: Pointer to a string of 0 and 1 chars
  *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
+ * Return: The converted number, or 0 if there is one or more chars
+ *         in the string b that is not 0 or 1, or b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0;
-	int len = 0;
+    unsigned int result = 0;
+    
+    if (b == NULL)
+	    return 0;
+    
+    for (; *b != '\0'; b++)
+    {
+	    if (*b != '0' && *b != '1')
+		return 0;
 
-	if (b == NULL)
-		return (0);
-
-	while (b[len])
-		len++;
-
-	while (len > 0)
-	{
-		len--;
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
-		n = n << 1;
-
-		if (b[len] == '1')
-			n = n | 1;
-	}
-
-	return (n);
+	result <<= 1;
+	if (*b == '1')
+		result |= 1; /* Use bitwise OR to set the least significant bit */
+    }
+    
+    return result;
 }
