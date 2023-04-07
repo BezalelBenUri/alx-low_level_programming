f4badec5c748ef8a100cfece43439caa74d71b26
PhenomenalAI <diamondinyang@gmail.com>
2023-04-07 11:28:24 -0700

    function that appends text at the end of a file...

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,9 +9,11 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
 
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
