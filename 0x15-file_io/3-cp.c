8053c3e0d1c8e11836ba07fcc843b07af811c3d4
PhenomenalAI <diamondinyang@gmail.com>
2023-04-10 11:36:05 -0700

    program that copies the content of a file to another file..

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,81 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Number of command line arguments
+ * @argv: Array of command line arguments
+ * Return: 0 on success, 97-100 on failure
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+
+	int result = copy_file(argv[1], argv[2]);
+
+	return (result);
+}
+
+/**
+ * copy_file - Copies contents of one file to another
+ * @file_from: Source file name
+ * @file_to: Destination file name
+ * Return: 0 on success, 97-100 on failure
+ */
+int copy_file(const char *file_from, const char *file_to)
+{
+	int file_from_file, file_to_file;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	file_from_file = open(file_from, O_RDONLY);
+	if (file_from_file == -1)
+	{
+		print_error_and_exit("Error: Can't read from file", file_from, 98);
+	}
+
+	file_to_fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |
+			S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (file_to_file == -1)
+	{
+		print_error_and_exit("Error: Can't write to file", file_to, 99);
+	}
+
+	while ((bytes_read = read(file_from_file, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(file_to_file, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			print_error_and_exit("Error: Can't write to file", file_to, 99);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		print_error_and_exit("Error: Can't read from file", file_from, 98);
+	}
+
+	if (close(file_from_file) == -1 || close(file_to_file) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
+		exit(100);
+	}
+
+	return (0);
+}
+
+/**
+ * print_error_and_exit - Prints error message, file name,exit code
+ * @error_message: Error message
+ * @file_name: File name
+ * @exit_code: Exit code
+ */
+void print_error_and_exit(const char *error_message,
+		const char *file_name, int exit_code)
+{
+	dprintf(STDERR_FILENO, "%s %s\n", error_message, file_name);
+	exit(exit_code);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,10 +10,15 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <stdio.h>
+#define BUFFER_SIZE 1024
 
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int append_text_to_file(const char *filename, char *text_content);
+
+int copy_file(const char *file_from, const char *file_to);
+void print_error_and_exit(const char *error_message, const char *file_name, int exit_code);
 
 #endif
