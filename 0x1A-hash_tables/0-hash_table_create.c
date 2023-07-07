3c80bf3bad2f806e2d90e89c8e3896a66542a42e
PhenomenalAI <diamondinyang@gmail.com>
2023-07-07 12:54:26 -0700

    create has table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,26 +1,36 @@
+#include <stdlib.h>
 #include "hash_tables.h"
 
 /**
- * hash_table_create - create a hash table
- * @size: size of the table
- * Return: pointer to the table
- **/
+ * hash_table_create - Creates a hash table
+ * @size: The size of the array
+ *
+ * Return: A pointer to the newly created hash table,
+ *         or NULL if something went wrong
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *tabs;
+	hash_table_t *ht;
+	unsigned long int i;
 
-	if (size == 0)
-		return (NULL);
-	tabs = malloc(sizeof(hash_table_t));
-	if (tabs == NULL)
+	/* Allocate memory for the hash table */
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
 
-	tabs->size = size;
-	tabs->array = calloc(size, sizeof(hash_node_t *));
-	if (tabs->array == NULL)
+	/* Allocate memory for the array */
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 	{
-		free(tabs);
+		free(ht);
 		return (NULL);
 	}
-	return (tabs);
+
+	ht->size = size;
+
+	/* Initialize each element of the array to NULL */
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
 }
