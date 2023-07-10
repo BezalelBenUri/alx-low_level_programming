f664979ef458c5149328f309e4e53386b277dc28
PhenomenalAI <diamondinyang@gmail.com>
2023-07-10 04:30:03 -0700

    add elements to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,12 +1,53 @@
 #include "hash_tables.h"
 
 /**
- * key_index - generates the key using hash_djb2
- * @key: pointer the key
- * @size: size table
- * Return: index key
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	return (hash_djb2(key) % size);
+	hash_node_t *recent;
+	char *val_copy;
+	unsigned long int index, i;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	val_cpy = strdup(value);
+	if (val_cpy == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = val_cpy;
+			return (1);
+		}
+	}
+
+	recent = malloc(sizeof(hash_node_t));
+	if (recent == NULL)
+	{
+		free(val_cpy);
+		return (0);
+	}
+	recent->key = strdup(key);
+	if (recent->key == NULL)
+	{
+		free(recent);
+		return (0);
+	}
+	recent->value = val_cpy;
+	recent->next = ht->array[index];
+	ht->array[index] = recent;
+
+	return (1);
 }
