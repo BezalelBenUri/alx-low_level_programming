44fae96939192abdccbaef270353268fad73442e
PhenomenalAI <diamondinyang@gmail.com>
2023-07-10 03:50:28 -0700

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -22,7 +22,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	for (n = 0; n < size; i++)
+	for (n = 0; n < size; n++)
 		(tab->array)[n] = NULL;
 	return (tab);
 }
