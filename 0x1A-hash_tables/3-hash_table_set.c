ea15077f28b82b63743820d324a08ca36eb13635
PhenomenalAI <diamondinyang@gmail.com>
2023-07-10 05:14:32 -0700

    add elements to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,14 +12,14 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
-	char *value_copy;
+	char *val_cp;
 	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	value_copy = strdup(value);
-	if (value_copy == NULL)
+	val_cp = strdup(value);
+	if (val_cp == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
@@ -28,7 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
+			ht->array[i]->value = val_cp;
 			return (1);
 		}
 	}
@@ -36,7 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
-		free(value_copy);
+		free(val_cp);
 		return (0);
 	}
 	new->key = strdup(key);
@@ -45,7 +45,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new);
 		return (0);
 	}
-	new->value = value_copy;
+	new->value = val_cp;
 	new->next = ht->array[index];
 	ht->array[index] = new;
 
