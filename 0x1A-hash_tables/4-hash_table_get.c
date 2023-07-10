a238859bf7257f1ee12765335f9cb6ef29cc773f
PhenomenalAI <diamondinyang@gmail.com>
2023-07-10 05:35:35 -0700

    retrieves a value associated with a key.

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,19 +11,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *node;
-	unsigned long int index;
+	hash_node_t *nd;
+	unsigned long int adrs;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	if (index >= ht->size)
+	adrs = key_adrs((const unsigned char *)key, ht->size);
+	if (adrs >= ht->size)
 		return (NULL);
 
-	node = ht->array[index];
-	while (node && strcmp(node->key, key) != 0)
-		node = node->next;
+	nd = ht->array[adrs];
+	while (nd && strcmp(nd->key, key) != 0)
+		nd = nd->next;
 
-	return ((node == NULL) ? NULL : node->value);
+	return ((nd == NULL) ? NULL : nd->value);
 }
